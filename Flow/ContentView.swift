97692b098f9aefe40ee48d8488ec48b8f8610d26
Raditya <raditya.learner@gmail.com>
2022-09-02 15:19:41 +0800

    Initial Commit

---
+++ Flow/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Flow
+//
+//  Created by CTSS Students on 2/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Flow/FlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowApp.swift
+//  Flow
+//
+//  Created by CTSS Students on 2/9/22.
+//
+
+import SwiftUI
+
+@main
+struct FlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
